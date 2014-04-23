aa8c55f49b277ec0847f4fc9c6074796b226ea42
Prosserc <prosser_chris@yahoo.co.uk>
2014-04-23 19:43:23 +0100

    Update test_checkmate2.py
    corrected third move.

--- test_checkmate2.py
+++ test_checkmate2.py
@@ -7,10 +7,8 @@ game = Game()
 # set up game to required position...
 game.take_turn('white', 'e7e5')
 game.take_turn('black', 'g2g4')
-game.take_turn('white', 'e5f4')
-game.take_turn('black', 'h2h3') # tmp not reqd once conds in
-game.take_turn('white', 'a7a5') # time filler
-game.take_turn('black', 'f2f3') # just delete two above when conds in
+game.take_turn('white', 'e5e4')
+game.take_turn('black', 'f2f3')
 
 print('\nControl going back to user.')
 print('hint: ready for checkmate if white queen does "d8h4"')
