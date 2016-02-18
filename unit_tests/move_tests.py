9abae5006488a2a5d036ca99e8151ac2777e09a0
Prosserc <prosser_chris@yahoo.co.uk>
2016-02-18 09:28:37 +0000

    started move tests

---
+++ unit_tests/move_tests.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import unittest, game, move
+
+
+class TestUtils(unittest.TestCase):
+
+
+    def setUp(self):
+        self.test_game = Game();
+
+
+    def check_initial_move(self):
+
+        # set up specific to this test...
+        self.test_game.current_team = game.TEAMS['w']
+        occupied, our_team, their_team = self.test_game.get_occupied()
+        piece_ref = selftest_game.board.get_piece_ref(2, 1)
+
+        if piece_ref:
+            piece = self.test_game.pieces[piece_ref]
